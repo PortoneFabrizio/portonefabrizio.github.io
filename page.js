3b91d55f654eb6d4794cc53b5667b5ffdf1d73d7
Portone Fabrizio <Fabri@MBP-di-Fabri.homenet.telecomitalia.it>
2018-06-27 13:54:10 +0200

    initial commit

---
+++ page.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('.icon').click(function(){
+          $('.icon').toggleClass('active');
+          $('.list').toggleClass('active');
+        })
+    $('.list li').click(function(){
+          $('.icon').toggleClass('active');
+          $('.list').toggleClass('active');
+        })
+ 
+//one page scroll ------------
+    
+    $(".list li a[href^='#']").on('click', function(e){
+        
+        target = this.hash; 
+        e.preventDefault();
+        
+        $('html,body').animate({
+        scrollTop : $(this.hash).offset().top
+        }, 600)
+    })
+    
+setTimeout(function(){
+ $('body').addClass('loaded');
+ }, 1500);
+    
+    
+})
